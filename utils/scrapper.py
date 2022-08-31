8d6e25d5f4c8978e4ddd64128d4f5bc06a47a6b4
engr-solution <dah.solu.tion7@gmail.com>
2022-08-31 19:18:00 +0100

    initial commit

---
+++ utils/scrapper.py
@@ -0,0 +1,65 @@
+# IMPORT NECCESSARY LIB
+import os
+import time
+from utils.formatter import format_description_text
+from utils.tweet import tweet
+
+
+
+trigger_Words = ['Trailer', 'Chat', 'Reveal', 'Announcement', 'Overview', 'Teaser', 'Adventure', 'Year', 'Update', 'Hearthstone']
+c_path = os.getcwd()
+
+def scrape_youtube(driver, WebDriverWait, By, EC):
+    driver.implicitly_wait(10)
+    driver.get('https://www.youtube.com/c/Hearthstone/videos')
+
+    videos = WebDriverWait(driver, 30).until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, "a.yt-simple-endpoint.style-scope.ytd-grid-video-renderer")))
+    time.sleep(0.5)
+
+    latest_video = None
+
+    for video in videos: 
+        print(video.text)
+        tweeted = False
+        if latest_video != None:
+            break
+        with open(c_path +"/data/data.txt") as f:
+            for line in f:
+                if line.strip() == video.get_attribute('href'):
+                    tweeted = True
+                    
+        if tweeted:
+            continue
+        title = video.text
+        print(title)
+        for word in trigger_Words:
+            if word.lower() in title.lower():
+                with open(c_path +"/data/data.txt", "a") as file:
+                    file.write(video.get_attribute('href') + '\n')
+                latest_video = video.get_attribute('href')
+                print(word)
+                break
+
+    if latest_video == None:
+        return print('No Latest video......')
+    else:
+        driver.get(latest_video)
+        
+        time.sleep(5)
+
+        intro = '📢--- New video spotted ---📢'
+        description = WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "span.style-scope.yt-formatted-string"))).text
+        url = driver.current_url
+        desc = format_description_text(description)
+
+        text = f"{intro}\n\n📺 {title}\n\n\"{desc}\"\n\nSource: {url}"
+
+        print('Youtube age...........................#####################################################')
+        print(text)
+
+        # UPLOAD TO TWITTER
+        tweet(text)
+        
+        time.sleep(5)
+        driver.quit()
+
---
+++ utils/tweet.py
@@ -0,0 +1,26 @@
+import os
+import tweepy
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# TWITTER API CREDENTIALS 
+consumerKey = os.environ.get("CONSUMER_KEY")
+consumerSecret = os.environ.get("CONSUMER_SECRET")
+accessToken = os.environ.get("ACCESS_TOKEN")
+accessTokenSecret = os.environ.get("ACCESS_TOKEN_SECRET")
+
+auth = tweepy.OAuth1UserHandler(
+    consumerKey,
+    consumerSecret,
+    accessToken,
+    accessTokenSecret
+)
+client = tweepy.API(auth)
+
+# The app and the corresponding credentials must have the Write permission
+def tweet(tweet):
+    response = client.update_status(
+        status=tweet
+    )
+   
