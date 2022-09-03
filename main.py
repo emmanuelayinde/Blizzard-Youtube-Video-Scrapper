cac8e3b6481a5441a518b1ed9407bb09b8dc7a1a
engr-solution <dah.solu.tion7@gmail.com>
2022-09-03 21:44:50 +0100

    add worker to Profile

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
 
-alive()
+# alive()
 
 while True:
     driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)            
